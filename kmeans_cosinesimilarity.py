ad6da66e0ee9896fb0fa73c7425940c0321287f5
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2022-01-25 15:30:51 +0200

    Add files via upload

--- kmeans_cosinesimilarity.py
+++ kmeans_cosinesimilarity.py
@@ -1,5 +1,4 @@
 
-
 import gensim
 import gensim.downloader as api
 from gensim.models import Word2Vec as w2v
@@ -13,6 +12,8 @@ import warnings
 import numpy as np
 
 from sklearn import *
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import pairwise_distances
 
 import os
 
@@ -26,12 +27,12 @@ def embed(uid):
     warnings.filterwarnings('ignore')
 
 
-    ratings_df = pd.read_csv('BX-Book-Ratings.csv')
+    ratings_df = pd.read_csv('/home/dagkla/Documents/BX-Book-Ratings.csv')
     ratings_df = ratings_df.loc[ratings_df['uid']==uid]
     ratings_df['isbn'] = ratings_df['isbn'].map(lambda x: x.strip())
     # print(len(ratings_df.isbn))
 
-    books_df = pd.read_csv('BX-Books.csv')
+    books_df = pd.read_csv('/home/dagkla/Documents/BX-Books.csv')
     # print(books_df.columns)
     books_df = books_df.drop(['book_title', 'book_author', 'year_of_publication', 'publisher', 'category'], axis='columns')
     books_df['isbn'] = books_df['isbn'].map(lambda x: x.strip())
@@ -56,18 +57,15 @@ def embed(uid):
         print(j)
         print("Va8mos : " + str(myratings[i]))
         i+=1
-    '''
-    AYTA MONO STHN ARXH
 
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    '''logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     corpus = api.load('text8')
     print(inspect.getsource(corpus.__class__))
     print(inspect.getfile(corpus.__class__))
     model = w2v(corpus)
-    model.save('.\\readyvocab.model')
+    model.save('readyvocab.model')'''
 
-    AYTA MONO STHN ARXH
-    '''
     model = w2v.load('readyvocab.model')
     processed_sentences = []
     for sentence in mtuple:
@@ -91,15 +89,33 @@ def embed(uid):
         df_input.fillna(value=0.0,inplace=True)
         df_input[str(i)].replace(to_replace=0,value=df_input[str(i)].mean(),inplace=True )
         print(df_input[str(i)])
+
+    my_kmeans(df=df_input,k=2)
     #np.any(np.isnan(df_input))
     #np.all(np.isfinite(df_input))
     #X_train, X_test, y_train, y_test = train_test_split(X, y,shuffle=False)
     '''ta exw ola ws ka8eta dianusmata'''
 
 
-def my_kmeans(k = 2, df):
+def my_kmeans(df,k = 2):
     #arxikopoihsh kentrweidwn
-    my_centroids = pd.DataFrame(np.random.rand(,))
+    print(df)
+    choose = np.random.randint(df.shape[1] , size=k)
+    my_centroids = []
+    print()
+    for i in range(0,k):
+        my_centroids.append( df[str(choose[i])] )
+    print(my_centroids)
+    i = 0
+    for i in range(0,df.shape[1]):
+        if i in choose:
+            pass
+        else:
+            similarities = []
+            for j in range(0,len(my_centroids)):
+                #edw paizei 8ema
+                similarities.append( cosine_similarity([my_centroids[j] ,df[str(i)]] ) )
+            print(similarities)
 
 def list2string(s):
     strl = ""
